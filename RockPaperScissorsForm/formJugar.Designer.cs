0c3f24394017aa14786f291fbb127deb4d341ae9
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-06-14 11:18:53 -0300

    update 14/06

--- RockPaperScissorsForm/formJugar.Designer.cs
+++ RockPaperScissorsForm/formJugar.Designer.cs
@@ -28,18 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formJugar));
+            btnPiedra = new Button();
+            btnPapel = new Button();
+            btnTijera = new Button();
+            lblNombre = new Label();
+            tbxNombre = new TextBox();
+            txbCpu = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
             SuspendLayout();
+            // 
+            // btnPiedra
+            // 
+            btnPiedra.BackgroundImage = (Image)resources.GetObject("btnPiedra.BackgroundImage");
+            btnPiedra.BackgroundImageLayout = ImageLayout.Zoom;
+            btnPiedra.Cursor = Cursors.Hand;
+            btnPiedra.Location = new Point(44, 111);
+            btnPiedra.Name = "btnPiedra";
+            btnPiedra.Size = new Size(95, 100);
+            btnPiedra.TabIndex = 0;
+            btnPiedra.UseVisualStyleBackColor = true;
+            btnPiedra.Click += btnPiedra_Click;
+            // 
+            // btnPapel
+            // 
+            btnPapel.BackgroundImage = (Image)resources.GetObject("btnPapel.BackgroundImage");
+            btnPapel.BackgroundImageLayout = ImageLayout.Zoom;
+            btnPapel.Cursor = Cursors.Hand;
+            btnPapel.Location = new Point(169, 111);
+            btnPapel.Name = "btnPapel";
+            btnPapel.Size = new Size(95, 100);
+            btnPapel.TabIndex = 3;
+            btnPapel.UseVisualStyleBackColor = true;
+            // 
+            // btnTijera
+            // 
+            btnTijera.BackgroundImage = (Image)resources.GetObject("btnTijera.BackgroundImage");
+            btnTijera.BackgroundImageLayout = ImageLayout.Zoom;
+            btnTijera.Cursor = Cursors.Hand;
+            btnTijera.Location = new Point(292, 111);
+            btnTijera.Name = "btnTijera";
+            btnTijera.Size = new Size(95, 100);
+            btnTijera.TabIndex = 4;
+            btnTijera.UseVisualStyleBackColor = true;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(133, 19);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(169, 25);
+            lblNombre.TabIndex = 5;
+            lblNombre.Text = "Ingrese su nombre";
+            // 
+            // tbxNombre
+            // 
+            tbxNombre.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbxNombre.Location = new Point(133, 47);
+            tbxNombre.Name = "tbxNombre";
+            tbxNombre.Size = new Size(169, 29);
+            tbxNombre.TabIndex = 6;
+            // 
+            // txbCpu
+            // 
+            txbCpu.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txbCpu.Location = new Point(44, 267);
+            txbCpu.Multiline = true;
+            txbCpu.Name = "txbCpu";
+            txbCpu.Size = new Size(343, 61);
+            txbCpu.TabIndex = 7;
+            txbCpu.Text = "Bienvenido usuario - Aqui veras la selección de la CPU";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 3000;
+            timer1.Tick += timer1_Tick;
             // 
             // formJugar
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
             ClientSize = new Size(427, 436);
+            Controls.Add(txbCpu);
+            Controls.Add(tbxNombre);
+            Controls.Add(lblNombre);
+            Controls.Add(btnTijera);
+            Controls.Add(btnPapel);
+            Controls.Add(btnPiedra);
             Name = "formJugar";
             Text = "formJugar";
+            Load += formJugar_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnPiedra;
+        private Button btnPapel;
+        private Button btnTijera;
+        private Label lblNombre;
+        private TextBox tbxNombre;
+        private TextBox txbCpu;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- RockPaperScissorsForm/formJugar.cs
+++ RockPaperScissorsForm/formJugar.cs
@@ -7,14 +7,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RockPaperScissors;
 
 namespace RockPaperScissorsForm
 {
     public partial class formJugar : Form
     {
+        Jugadores nuevoJugador;
+        char SeleccionJugador;
+        char SeleccionCpu;
+        int contarVictorias = 0;
+
         public formJugar()
         {
             InitializeComponent();
         }
+        private void formJugar_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnPiedra_Click(object sender, EventArgs e)
+        {
+            SeleccionJugador = 'p';
+            SeleccionCpu = OpcionRival();
+            MostrarSeleccionCpu();
+        }
+
+
+        private char OpcionRival()
+        {
+            char[] opciones = new char[] { 'r', 'p', 's' };
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, opciones.Length);
+
+            return opciones[randomIndex];
+        }
+        private void MostrarSeleccionCpu()
+        {
+            switch (OpcionRival())
+            {
+                case 'r':
+                    txbCpu.Text = "CPU HA ELEGIDO PIEDRA";
+                    break;
+                case 'P':
+                    txbCpu.Text = "CPU HA ELEGIDO PAPEL";
+                    break;
+                case 'S':
+                    txbCpu.Text = "CPU HA ELEGIDO TIJERA";
+                    break;
+            }
+        }
+       
+
+        private void DevolverGanador(char seleccionUser, char seleccionRival)
+        {
+            if (seleccionUser == seleccionRival)
+            {
+                MessageBox.Show("Es un empate!");
+            }
+            else
+            {
+                switch (seleccionUser)
+                {
+                    case 'r':
+                        if (seleccionRival == 'p')
+                        {
+                            MessageBox.Show("CPU GANA!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("USUARIO GANA!");
+                            contarVictorias++;
+                        }
+                        break;
+                    case 'p':
+                        if (seleccionRival == 's')
+                        {
+                            MessageBox.Show("CPU GANA!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("USUARIO GANA!");
+                            contarVictorias++;
+
+                        }
+                        break;
+                    case 's':
+                        if (seleccionRival == 'r')
+                        {
+                            MessageBox.Show("CPU GANA!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("USUARIO GANA!");
+                            contarVictorias++;
+
+                        }
+                        break;
+                }
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DevolverGanador(SeleccionJugador, SeleccionCpu);
+            timer1.Stop();
+        }
     }
+
+
+
 }
